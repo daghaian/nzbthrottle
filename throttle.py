3f33f3af2af8a1314c4352a73fd8602bf0802ae0
davidaghaian@student.cccd.edu <daghaian@student.cccd.edu>
2018-08-11 16:45:29 -0700

    Renamed main module

---
+++ throttle.py
@@ -0,0 +1,43 @@
+import plex
+import nzb
+import time
+import logging.handlers
+
+#Initialize Logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# create a file handler
+# Max Log Size - 10 MB
+# Max Log Count - 1
+fh = logging.handlers.RotatingFileHandler('./nzbthrottle.log',maxBytes=10 * 1024 * 1024 , backupCount=1)
+fh.setLevel(logging.INFO)
+
+# create console handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s-%(module)-6s: %(levelname)-8s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S ')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+p = plex.PlexServer()
+n = nzb.NZB()
+
+while(1):
+    logger.info("Requesting active stream count...")
+    active_streams = p.get_active_streams()
+    if(active_streams != None):
+        logger.info("Current stream count: %d",active_streams)
+        if(active_streams > 0):
+            logger.info("There are currently active streams. Proceeding to throttle NZB")
+
+    logger.info("Sleeping for %d seconds before checking again",p.get_interval())
+    time.sleep(p.get_interval())
+
+

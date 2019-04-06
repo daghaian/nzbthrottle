974b6ec1ac0267bbe8458b04fec4eb3ce700a88d
davidaghaian@student.cccd.edu <16483722+daghaian@users.noreply.github.com>
2019-04-06 00:27:00 -0700

    Added notify statements and refactored names of nzb and plex server objects

--- throttle.py
+++ throttle.py
@@ -13,57 +13,68 @@ def start_monitor():
         lastThrottleState = False
         last_active_streams = 0
         #Initially set speed of nzbget to unthrottle or the user defined maxSpeed
-        n.set_start_speed()
+        nzbServer.set_start_speed()
         while (1):
             logger.info("Requesting active stream count...")
-            active_streams = p.get_active_streams()
-            state = n.get_current_throttle_status()
+            active_streams = plexServer.get_active_streams()
+            state = nzbServer.get_current_throttle_status()
             if(state == False and lastThrottleState == True):
                     logger.debug("Previous state of throttle flag was True but currently not throttled, changing to False!")
                     lastThrottleState = False
             elif(state == True and lastThrottleState == False):
                     logger.debug("Previous state of throttle flag was False but currently we are throttled, changing to True!")
-                    last_active_streams = p.get_active_streams()
+                    last_active_streams = plexServer.get_active_streams()
                     lastThrottleState = True
-
             if (active_streams != None):
                 logger.info("Current stream count: %d", active_streams)
                 if (lastThrottleState):
                     if (active_streams == 0):
                         logger.info("Streams are 0 and we are currently throttled. Lifting the limit")
-                        if(n.throttle_streams(active_streams) == True):
-                            x.notifiy("Streams are 0 and we are currently throttled. Lifting the limit")
+                        throttleResponse = nzbServer.throttle_streams(active_streams)
+                        if(throttleResponse != -1):
                             lastThrottleState = False
                             last_active_streams = active_streams
                             logger.info("Throttle lifted successfully")
+                            try:
+                                notifyClient.notify("Streams are 0 and we were throttled. Lifted the limit")
+                            except Exception as e:
+                                logger.error("Error encountered when attempting to send notification")
                     elif(active_streams != last_active_streams):
                         logger.info("Already throttled, but stream count has changed, adjusting speed")
-                        if (n.throttle_streams(active_streams) == True):
-
+                        throttleResponse = nzbServer.throttle_streams(active_streams)
+                        if (throttleResponse != -1):
                             last_active_streams = active_streams
                             logger.info("Speed throttling adjusted successfully")
+                            try:
+                                notifyClient.notify("Stream count has changed to {}, adjusted speed to: {} KB/s".format(active_streams,throttleResponse))
+                            except Exception as e:
+                                logger.error("Error encountered when attempting to send notification")
                     else:
                         logger.info("Already throttled with no change. Continuing to monitor.")
                 else:
                     if (active_streams > 0):
                         logger.info("There are currently active streams. Proceeding to throttle NZB")
-                        if (n.throttle_streams(active_streams) == True):
-
+                        throttleResponse = nzbServer.throttle_streams(active_streams)
+                        if (throttleResponse != -1):
                             logger.info("NZB throttled successfully")
                             lastThrottleState = True
                             last_active_streams = active_streams
+                            try:
+                                notifyClient.notify("Currently {} active streams, throttled to: {} KB/s".format(active_streams,throttleResponse))
+                            except Exception as e:
+                                logger.error("Error encountered when attempting to send notification")
                         else:
-                            logger.error("Something went wrong when attemping to throttle NZB")
+                            logger.error("Something went wrong when attempting to throttle NZB")
                     else:
                         logger.info("No active streams and not previously throttled. Ensuring correct unthrottle speed is set")
-                        if(n.throttle_streams(0) == True):
-                            x.notifiy("No active streams and not previously throttled. Ensuring correct unthrottle speed is set")
+                        throttleResponse = nzbServer.throttle_streams(0)
+                        if(throttleResponse != -1):
                             logger.info("Unthrottled speed set correctly")
-   
-            logger.info("Sleeping for %d seconds before checking again", p.get_interval())
-            time.sleep(p.get_interval())
+            logger.info("Sleeping for %d seconds before checking again", plexServer.get_interval())
+            time.sleep(plexServer.get_interval())
     except Exception as e:
-        logger.error("Start monitor encountered exception. Trying again in 60 seconds")
+        logger.exception("Start monitor encountered exception. Trying again in 60 seconds")
+
         time.sleep(60)
 
 #=======================================================#
@@ -96,11 +107,11 @@ logger.addHandler(fh)
 logger.addHandler(ch)
 
 # initialize notification client
-x = notification.NotificationClient()
+notifyClient = notification.NotificationClient()
 # initialize plex server
-p = plex.PlexServer()
+plexServer = plex.PlexServer()
 # initialize NZB server
-n = nzb.NZB()
+nzbServer = nzb.NZB()
 
 while(1):
     start_monitor()
